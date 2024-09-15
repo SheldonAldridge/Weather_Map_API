cedb40136cace78fc7b83e87266cd5d493e36309
Sheldon <sheldonaldridge@yahoo.com>
2024-09-15 11:25:42 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -6,10 +6,7 @@ const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lo
 
 
 let dataArray = []
-//let randomData = [1,1,1,1,5,9,4,5,6,8,54,56,4,564,56,89,45,46,8]
 let allData = []
-
-
 
 function fetchData(){
 fetch(url)
@@ -26,19 +23,11 @@ fetch(url)
         console.log('Data fetched:', data)   
         currentState(data)
         displayDataArray(data)
-        
+
     })
     .catch(error =>{
         console.log('Error fetching data: ', error);
     })
-}
-
-function refreshFetch(){
-    window.setInterval(() =>{
-        fetchData()
-        console.log('url fetched')
-    },60000)
-
 }
 
 function currentState(data){
@@ -86,40 +75,185 @@ function displayDataArray(){
 
 });
 
-
 }
 
 function filterData(data){
 
     for (data of allData) {
-        const existData = dataArray.find((item) => 
-            item.wind.deg == data.wind.deg ||
-            item.wind.speed == data.wind.speed);
+        const existData = dataArray.find((item) => item.wind.speed === data.wind.speed || item.wind.deg === data.wind.deg);
 
         if(!existData){
-            console.log("Adding new data to dataArray", data);
             dataArray.push(data)
         }
-        else{
-            console.log("data matches", data);
-        }
-        
     }
+}
+
+//Create Interval Input function
+function initializeIntervalControl(){
     
+    let dataColEl = document.querySelector('.col-interval')
+    let interval = [' ','Seconds','Minutes','Hours']
+
+    let intervalInputEl = document.createElement('input');
+    intervalInputEl.setAttribute('type','text')
+    intervalInputEl.classList.add('interval-input')
+
+    intervalInputEl.onkeydown = function(event){
+        if(isNaN(event.key) && event.key !== 'Backspace'){
+            event.preventDefault();
+        }
+    };
+    
+    let intervalSelectEl = document.createElement('select');
+    intervalSelectEl.classList.add('select-interval')
+    let intervalBtnEl = document.createElement('button');
+    intervalBtnEl.classList.add('interval-Btn')
+    intervalBtnEl.innerText = 'Set Data Interval'
+   
+    //for looop to create options from Intercal array
+    for(let i = 0; i < interval.length; i++){
+        let intervalOptiontEl = document.createElement('option');
+        intervalOptiontEl.innerHTML = `${interval[i]}`
+        intervalSelectEl.append(intervalOptiontEl);
+    }
+
+    dataColEl.append(intervalInputEl);
+    dataColEl.append(intervalSelectEl);
+    dataColEl.append(intervalBtnEl);
 }
 
-//function to set value of pulling data intervals
-function dataIntervals(data){
+/*
+function setupIntervalButton(input,select,button){
+    console.log('Setting up event listener'); // Debugging line
+    console.log('Button element inside setup:', button); // Debugging line
+    
+    button.addEventListener('click', () =>{
+        const intervalValue = input.value;
+        const selectedInterval = select.value
 
+        if (!intervalValue && !selectedInterval) {
+            alert("Please provide numeric value and select interval type");
+        } else if (!intervalValue) {
+            alert("Please provide numeric value for interval");
+        } else if (!selectedInterval) {
+            alert("Please select interval type");
+        } else{
+            console.log(`Interval set to ${intervalValue} ${selectedInterval}`);
+            
+        }
+
+        if(selectedInterval === "Seconds"){
+            console.log(intervalValue)
+            return intervalValue * 1000
+        }
+        else if(selectedInterval === "Minutes"){
+            console.log(intervalValue)
+            return intervalValue * 60000
+        }
+        else if(selectedInterval === "Hours"){
+            console.log(intervalValue)
+            return intervalValue * 60 * 60 * 1000
+        }else{
+            return intervalValue * 1000
+        }
+
+    })
+}
+*/
+
+initializeIntervalControl()
+
+const intervalLookup = {
+    Seconds: 1000,
+    Minutes: 60000,
+    Hours: 60 * 60 * 10000,
 }
 
-//function that checks userInput of geo-location
-//function that breaks objects apart into
+const input = document.querySelector('.interval-input');
+const select = document.querySelector('.select-interval');
+const button = document.querySelector('.interval-Btn');
+
+let fetchInterval = null;
+
+function isValidInterval(intervalValue, selectedInterval){
+    if (!intervalValue && !selectedInterval) {
+        alert('Please provide numeric value and select interval type');
+        return false;
+      } else if (!intervalValue) {
+        alert('Please provide numeric value for interval');
+        return false;
+      } else if (!selectedInterval) {
+        alert('Please select interval type');
+        return false;
+      }
+      return true
+}
+
+const setupIntervalButton = (input,select,button) =>{
+    console.log('Setting up event listener');
+    console.log('Button elements inside setup:', button);
+
+    button.addEventListener('click', () =>{
+    const intervalValue = input.value;
+    const selectedInterval = select.value;
+
+    //validation check
+    if(!isValidInterval(intervalValue, selectedInterval)) return;
+
+    if(fetchInterval){
+        clearInterval(fetchInterval);
+        console.log('Previous interval cleared')
+    }
+
+    const intervalMilliseconds = intervalValue * (intervalLookup[selectedInterval] || 1000);
+    console.log('Interval in milliseconds:', intervalMilliseconds);
+
+    
+
+    setupDynamicIntervalChange(input, select);
+    refreshFetch(intervalMilliseconds);
+
+    });
+}
+
+const refreshFetch = (intervalMilliseconds) =>{
+    setInterval(() =>{
+        fetchData();
+        console.log('url fetched');
+    }, intervalMilliseconds)
+}
+
+const setupDynamicIntervalChange = (input,select) =>{
+
+    input.addEventListener('input', () =>{
+        const intervalValue = input.value;
+        const selectedInterval = select.value;
+
+        if(isValidInterval(intervalValue, selectedInterval)){
+            document.querySelector('.interval-Btn');
+            return
+        }
+
+    });
+
+    select.addEventListener('change', () =>{
+        const intervalValue = input.value;
+        const selectedInterval = select.value;
+
+        if(isValidInterval(intervalValue, selectedInterval)){
+            document.querySelector('.interval-Btn');
+        }
+    });
+}
+
+const clearInterval = (fetchInterval) =>{
+    fetchInterval = ' '
+}
+
+setupIntervalButton(input, select, button);
 
 fetchData()
-refreshFetch()
 
-
-console.log("Filtered data: ", dataArray)
-console.log("All data: ", allData)
+// console.log(dataArray)
+// console.log(allData)
 
